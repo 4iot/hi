46c8e68e7ae3d0bac185b92a391743d64df71782
jgp <jgp@jgp.net>
2016-04-23 12:34:33 -0400

    Work on OS identification

--- python/HiMessage.py
+++ python/HiMessage.py
@@ -1,18 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
 import json
 import urllib2
 import platform
 import cpuinfo
 import datetime
 import time
+import osinfo
 
 class HiMessage:
-    "Send a Hi message to the server"
+    "Sends and manages a Hi message to the server"
+    
+    __debug = False
     
     def __init__(self, msg=None, seq=None):
         self.reset()
-        if ( msg != None ): 
+        if ( msg != None ) : 
             self.__data['msg'] = msg
-        if ( seq != None ): 
+        if ( seq != None ) : 
             self.__data['seq'] = seq
 
     def reset(self):
@@ -22,16 +28,20 @@ class HiMessage:
         self.__data['timestamp'] = now.isoformat()
         info = cpuinfo.get_cpu_info()
         self.__data['cpu'] = format(info['brand'])
+        self.__data['os'] = osinfo.get_os_info()
         
     def send(self):
-        #req = urllib2.Request('http://localhost:8080/io.oplo.receiver/hi')
-        req = urllib2.Request('http://api.4iot.io/io.oplo.receiver/hi')
+        if (self.__debug == True):
+            url = 'http://localhost:8080/io.oplo.receiver/hi'
+        else:
+            url = 'http://api.4iot.io/io.oplo.receiver/hi'
+        req = urllib2.Request(url)
         req.add_header('Content-Type', 'application/json')
-        try :
+        try:
             response = urllib2.urlopen(req, json.dumps(self.__data))
             the_page = response.read()
             print the_page
-        except :
+        except:
             print "HTTP Post error"
 
     def add(self, key, value):
---
+++ python/osinfo/osinfo.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+import platform
+
+def get_os_info_linux():
+    rinfo = platform.linux_distribution()
+    if (rinfo == None):
+        info = None
+    else:
+        info = {}
+        info['os'] = 'Linux'
+        info['release'] = rinfo[0]
+        info['version'] = rinfo[1]
+        info['machine'] = rinfo[2]
+    
+    return info
+
+def get_os_info_windows():
+    return None
+    
+def get_os_info_mac():
+    rinfo = platform.mac_ver()
+    if (rinfo == None):
+        info = None
+    else:
+        info = {}
+        info['os'] = 'MacOS X'
+        info['release'] = rinfo[0]
+        info['version'] = rinfo[1]
+        info['machine'] = rinfo[2]
+    
+    return info
+
+def get_os_info():
+    info = None
+    
+    info = get_os_info_linux()
+    
+    if not info:
+        info = get_os_info_windows()
+        
+    if not info:
+        info = get_os_info_mac()
+    
+    return info
+    
+    
