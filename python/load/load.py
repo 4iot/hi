d6cbe992095a9ebd7abb139819df42b36b703030
jgp <jgp@jgp.net>
2016-04-25 17:37:33 -0400

    Work on OO this thing

--- python/load/load.py
+++ python/load/load.py
@@ -2,9 +2,51 @@
 # -*- coding: UTF-8 -*-
 
 import subprocess
+import sys
 
-def get_cpu_load():
-    res = subprocess.check_output(['sh', '-c', 'ps -e -o %cpu | awk \'{s+=$1} END {print s}\''])
-    return float(res)
+sys.path.append('../osinfo')
+from osinfo import OsInfo
 
-#print get_cpu_load()
+class Load:
+    
+    __osi = None
+    
+    def __init__(self):
+        self.__osi = OsInfo()
+
+    def getCpuLoad(self):
+        res = subprocess.check_output(['sh', '-c', 'ps -e -o %cpu | awk \'{s+=$1} END {print s}\''])
+        return float(res)
+
+    def getStorageStatus(self):
+        res = subprocess.check_output(['sh', '-c', 'df -k | grep "/dev/" | grep -v tmp']).splitlines()
+        drives = {}
+        index = 0;
+        count = len(res)
+        
+        while index < count:
+            current = res[index].split()
+            drive = {}
+            if (self.__osi.isMacOS() == True):
+                drive['dev'] = current[0]
+                drive['used'] = int(current[2])
+                drive['avail'] = int(current[3])
+                drive['mount'] = current[8]
+            elif (self.__osi.isLinux() == True):
+                drive['dev'] = current[0]
+                drive['used'] = current[2]
+                drive['avail'] = current[3]
+                drive['mount'] = current[5]
+            else:
+                print 'Error: unknown OS'
+            if (len(drive) > 0):
+                drives[drive['mount']] = drive
+                del drive['mount']
+            index += 1
+            
+        return drives
+
+l = Load()
+print l.getCpuLoad()
+print l.getStorageStatus()
+
--- python/osinfo/osinfo.py
+++ python/osinfo/osinfo.py
@@ -3,51 +3,75 @@
 
 import platform
 
-def get_os_info_linux(cinfo = None):
-    rinfo = platform.linux_distribution()
-    if (rinfo[1] == ''):
-        info = None
-    else:
-        info = {}
-        info['os'] = 'Linux'
-        info['dist'] = rinfo[0]
-        info['version'] = rinfo[1]
-        if (cinfo != None):
-            info['arch'] = cinfo['arch']
+class OsInfo:
+    "Operating System information"
+    
+    __isSet = False
+    __isMacOS = False
+    __isLinux = False
+    __isWindows = False
+
+    def getOsInfoLinux(self, cinfo = None):
+        rinfo = platform.linux_distribution()
+        if (rinfo[1] == ''):
+            info = None
         else:
-            info['arch'] = platform.processor()
-        info['kernel'] = platform.uname()[2]
-    
-    return info
+            info = {}
+            info['os'] = 'Linux'
+            info['dist'] = rinfo[0]
+            info['version'] = rinfo[1]
+            if (cinfo != None):
+                info['arch'] = cinfo['arch']
+            else:
+                info['arch'] = platform.processor()
+            info['kernel'] = platform.uname()[2]
+            self.__isLinux = True
+            self.__isSet = True
+            
+        return info
 
-def get_os_info_windows(cinfo = None):
-    return None
+    def getOsInfoWindows(self, cinfo = None):
+        return None
     
-def get_os_info_mac(cinfo = None):
-    rinfo = platform.mac_ver()
-    if (rinfo == None):
+    def getOsInfoMacOS(self, cinfo = None):
+        rinfo = platform.mac_ver()
+        if (rinfo == None):
+            info = None
+        else:
+            info = {}
+            info['os'] = 'MacOS'
+            info['dist'] = 'X'
+            info['version'] = rinfo[0]
+            info['arch'] = rinfo[2]
+            info['kernel'] = platform.uname()[2]
+            self.__isMacOS = True
+            self.__isSet = True
+    
+        return info
+
+    def getOsInfo(self, cinfo = None):
         info = None
-    else:
-        info = {}
-        info['os'] = 'MacOS'
-        info['dist'] = 'X'
-        info['version'] = rinfo[0]
-        info['arch'] = rinfo[2]
-        info['kernel'] = platform.uname()[2]
     
-    return info
+        print 'testing linux'
+        info = self.getOsInfoLinux(cinfo)
+        if not info:
+            print 'testing windows'
+            info = self.getOsInfoWindows(cinfo)
+        if not info:
+            print 'testing mac'
+            info = self.getOsInfoMacOS(cinfo)
+        return info
+    
+    def isMacOS(self):
+        if self.__isSet == False:
+            self.getOsInfo()
+        return self.__isMacOS
 
-def get_os_info(cinfo = None):
-    info = None
-    
-    info = get_os_info_linux(cinfo)
-    
-    if not info:
-        info = get_os_info_windows(cinfo)
-        
-    if not info:
-        info = get_os_info_mac(cinfo)
-    
-    return info
-    
-    
+    def isLinux(self):
+        if self.__isSet == False:
+            self.getOsInfo()
+        return self.__isLinux
+
+#osi=OsInfo()
+#print osi.isMacOS()
+#print osi.isLinux()
