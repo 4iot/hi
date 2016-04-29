f9fb40f240a280a99577d531777c400a4b34987d
jgp <jgp@jgp.net>
2016-04-29 09:10:10 -0500

    Fix errors in script

--- python/hi.sh
+++ python/hi.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
+# Simple message sender, you can add as many arguments as you want, they will be added as a message
+
+# This script can be inserted in your crontab:
+# $ sudo crontab -e
+#
+# Add:
+# */2 * * * * /home/pi/Documents/hi/python/hi.sh
+#
+# This will send a message every 2 minutes
+
+export PATH=$PATH:/sbin
+
 dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 python $dir/hi.py $*
